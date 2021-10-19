480d63e21489759b5fa81fccd935d64a0e1b59a7
propredator23 <christiandash@hotmail.com>
2021-10-19 13:15:51 -0700

    Bad Improvements
    The platforms spawn with the correct rotations but only in one type of wall

--- Assets/Scripts/Abilitites/ProjectilePlatform/ProjectilePlatform.cs
+++ Assets/Scripts/Abilitites/ProjectilePlatform/ProjectilePlatform.cs
@@ -19,7 +19,7 @@ public class ProjectilePlatform : MonoBehaviour
 
         if (n == Vector3.up)
         {
-            print("huh");
+            //print("huh");
             spawned.transform.position = collision.contacts[0].point + n * spawned.transform.localScale.x * .3f;
             spawned.transform.right = -direction;
             //spawned.transform.up = -direction;
@@ -27,9 +27,14 @@ public class ProjectilePlatform : MonoBehaviour
         }
         else 
         {
+            Vector3 right = Vector3.Cross(n, Vector3.up).normalized;
+            float ang = Vector3.Angle(right, -direction);
+            //print(ang);
             spawned.transform.position = collision.contacts[0].point + n * spawned.transform.localScale.z * .5f;
             spawned.transform.forward = n;
-            spawned.transform.localEulerAngles = new Vector3(0, 0, Vector3.Angle(Vector3.up, direction) - 90);
+            float angle = Vector3.Angle(Vector3.up, direction) - 90 * ((ang > 90) ? 1 : 2);
+            //print(angle);
+            spawned.transform.localEulerAngles = new Vector3(0, 0, angle) ;
         }
         Destroy(gameObject);
 
